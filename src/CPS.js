be6a8fc4713370897da9b8c8e58a6ebfb5303db7
Norbert Ursu <norbiu@gmail.com>
2016-11-05 23:30:04 +0200

    clean up

--- src/CPS.js
+++ src/CPS.js
@@ -5,9 +5,7 @@ import './CPS.css';
 class CPS extends Component {
   constructor(props) {
     super(props);
-
     this.state = { cpsText: '', cps: 0 }
-
     this.tsStart = 0;
   }
 
@@ -19,22 +17,16 @@ class CPS extends Component {
   }
 
   handleTextChange = (e) => {
-    this.setState({ cpsText: e.target.value });
-
-    if (e.target.value.length === 0) {
-      this.tsStart = 0;
-      this.setState({ cps: 0 })
-    }
-
     if (e.target.value.length === 1) {
       this.tsStart = Date.now();
       this.setState({ cps: 0 })
     }
 
+    this.setState({ cpsText: e.target.value });
   }
 
   handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && this.state.cpsText.length > 0) {
       this.getCPS();
     }    
   }
@@ -42,7 +34,13 @@ class CPS extends Component {
   render() {
     return (
       <div className="cps">
-        <input type="text" value={this.state.cpsText} onChange={this.handleTextChange} onKeyDown={this.handleKeyDown} placeholder="Type in a word and press Enter" />
+        <input
+          type="text"
+          value={this.state.cpsText}
+          onChange={this.handleTextChange}
+          onKeyDown={this.handleKeyDown}
+          placeholder="Type in a word and press Enter" />
+        
         <Result cps={this.state.cps} />
       </div>
     );
